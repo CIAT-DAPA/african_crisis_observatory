540bc7a992d140e9aa5033f2935f2d600d2f0906
bensonkenduiywo <bensonkemboi@gmail.com>
2023-04-27 12:30:55 -0500

    ---edits

--- auxiliary/MergeUNCHR_maps.R
+++ auxiliary/MergeUNCHR_maps.R
@@ -1,13 +1,15 @@
 g <- gc(reset = T); 
 rm(list = ls()) # Empty garbage collector
-root <- 'D:/OneDrive - CGIAR/Data/UNCHR/conflict'
+#root <- 'D:/OneDrive - CGIAR/Data/UNCHR/conflict'
+root <- '//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO/1.Data/Palmira/CSO/data'
+out <- '//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO/1.Data/Palmira/UNCHR'
 countries_lst <- c('BDI','DJI', 'ERI', 'ETH', 'KEN','RWA','SDN','SOM', 'SSD', 'TZA', 'UGA')
 region <- 'East_and_Horn_of_Africa'
 shps_list <- list()
 library(terra)
 for(i in 1:length(countries_lst)){
-  shp <- vect(paste0(root,'/',countries_lst[i],'/conflict_regular_clust.shp'))
-  labels <- read.csv(paste0(root,'/',countries_lst[i],'/conflict_cluster_text_description.csv'))
+  shp <- vect(paste0(root,'/',countries_lst[i],'/_results/cluster_results/conflict/conflict_regular_clust.shp'))
+  labels <- read.csv(paste0(root,'/',countries_lst[i],'/_results/cluster_results/conflict/conflict_cluster_text_description.csv'))
   shp <- merge(shp, labels[c('clust','label','short_label')], by='clust')
   shps_list[[i]] <- shp
 }
@@ -23,11 +25,16 @@ plot(shp, "label", col=rainbow(3), lwd=3, type="classes")
 #files <- list.files(root, recursive = T, full.names = T, pattern = '*.shp')
 #shps_list <- lapply(files, vect)
 
+wrld_shp <- terra::vect(paste0(root, "/_global/world_shapefile/all_country/all_countries.shp"))
+regionShp <- wrld_shp[wrld_shp$ISO3 %in% countries_lst,]
+regionShp <- sf::st_as_sf(regionShp)
 bdy <- sf::st_as_sf(shp)
 library(tmap)
 library(mapview)
 tmap_mode("view")
-map <- tm_shape(bdy)+
+map <- tm_shape(regionShp)+
+  tm_borders()+
+  tm_shape(bdy)+
   tm_fill(col= "cluster", palette="-YlOrRd", title='Conflict', labels = labs) +
   tm_compass(type = "8star", position = c("right", "top")) +#c('red','yellow','orange')
   tm_scale_bar(breaks = c(0, 50, 100), text.size = 1, width=1,
@@ -44,14 +51,9 @@ map <- tm_shape(bdy)+
 
 #tm_format("World")
 map
-out <- root
+
 tmap_save(map,  dpi= 600,  height=8, width=10, units="in",
-          filename=paste0(root,'/', region,'.png'))
+          filename=paste0(out,'/', region,'.png'))
 
 to_save <- shp[,  c("label", "short_label", "cluster")]
-writeVector(to_save, filename = paste0(root,'/', region,'.shp'))
-
-
-tree <- rast('D:/OneDrive - CGIAR/Data/UNCHR/tree_cover.tif')
-plot(tree)
-plot(shp, add=T)
+writeVector(to_save, filename = paste0(out,'/', region,'.shp'))
