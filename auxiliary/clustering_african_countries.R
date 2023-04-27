af17e467c5f8d86a2caece537836eed4105baeb6
bensonkenduiywo <bensonkemboi@gmail.com>
2023-04-27 14:05:18 -0500

    --Initial draft

--- auxiliary/clustering_african_countries.R
+++ auxiliary/clustering_african_countries.R
@@ -9,18 +9,32 @@ rm(list=ls(all=TRUE))
 require(pacman)
 pacman::p_load(terra, sf, exactextractr, tidyverse, FactoMineR)
 
-
+region <- 'East_and_Horn_of_Africa_zscored'
 #' define global vars and load inputs
 root <- "//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO/1.Data/Palmira/CSO/data/"
+out <- "//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO/1.Data/Palmira/UNCHR/"
 
 wrld_shp <- terra::vect(paste0(root, "/_global/world_shapefile/all_country/all_countries.shp"))
 
 af_shp <- wrld_shp[wrld_shp$CONTINENT == "Africa"] 
 
 countries_lst <- c('Burundi','Djibouti', 'Eritrea', 'Ethiopia', 'Kenya','Rwanda','Sudan','Somalia', 'South Sudan', 'Tanzania', 'Uganda')
+countries_iso <- c("BDI", "DJI", "ERI", "ETH", "KEN", "RWA", "SDN", "SOM", "SSD", "TZA", "UGA" )
+
 #' consider downloading lowest level because the world shapefile is missing some countries like SSD
 conflic_raw <- readxl::read_excel(paste0(root, "/_global/conflict/Africa_1997-2022_Jul08.xlsx"))
 cols <- c("EVENT_DATE", "YEAR", "EVENT_TYPE","SUB_EVENT_TYPE", "ACTOR1", "ACTOR2","FATALITIES", "LATITUDE", "LONGITUDE", "COUNTRY" ) 
+
+#load population density rasters 
+fls <- list.files(paste0(root, "/", countries_iso, "/", "population_density/" ), 
+                  pattern = "medn_popd.tif$",
+                  full.names = T)
+
+rsts <- lapply(fls, terra::rast)
+
+popd_af <- purrr::reduce(rsts, terra::mosaic, fun = "mean")
+
+terra::writeRaster(popd_af, paste0(out,"pop_af.tif"))
 
 #load and process conflict data (uptadted to JULY 2022)
 conflict_clean <- conflic_raw[conflic_raw$COUNTRY %in% countries_lst, cols ] 
@@ -42,7 +56,9 @@ grd<- grd[!is.na(sapply(st_intersects(grd, conflict_sf) , function(i){if(length(
 
 #' Add to conflict sf the grid id
 conflict_sf$grd_id <- sapply(st_intersects(conflict_sf, grd), function(i){ifelse(length(i)==0,NA, unlist(grd[i, "id"]))})
-
+conflict_sf$pop_dens <- terra::extract(popd_af, conflict_sf)$layer
+#remove ACLED location with pop density NA
+conflict_sf <- conflict_sf[!is.na(conflict_sf$pop_dens), ]
 #' min Max normalization
 minMax <- function(x){
   n <- x-min(x, na.rm = T)
@@ -60,18 +76,115 @@ zscore <- function(y, log=FALSE){
 
 #' by grid id, aggregate variables  and joint in a data.frame
 aa <- data.frame(id = sort(unique(conflict_sf$grd_id)),
-           EVENTS =  zscore(as.vector(by(conflict_sf$EVENT_TYPE, conflict_sf$grd_id, length))),#median(EVENTS, na.rm = T),
-           TYPE_RICHNESS = zscore(as.vector(by(conflict_sf$EVENT_TYPE, conflict_sf$grd_id, function(i){length(unique(i))}))),#max(TYPE_RICHNESS, na.rm = T),
-           SUBTYPE_RICHNESS = zscore(as.vector(by(conflict_sf$SUB_EVENT_TYPE, conflict_sf$grd_id, function(i){length(unique(i))}))),#max(SUBTYPE_RICHNESS, na.rm = T),
-           ACTOR1_RICHNESS = zscore(as.vector(by(conflict_sf$ACTOR1, conflict_sf$grd_id, function(i){length(unique(na.omit(i)))}))),#max(ACTOR1_RICHNESS, na.rm = T),
-           ACTOR2_RICHNESS = zscore(as.vector(by(conflict_sf$ACTOR2, conflict_sf$grd_id, function(i){length(unique(na.omit(i)))}))),#,
-           FATALITIES = zscore(as.vector(by(conflict_sf$FATALITIES, conflict_sf$grd_id, sum)))
+                 EVENTS =  (as.vector(by(conflict_sf$EVENT_TYPE, conflict_sf$grd_id, length))),#median(EVENTS, na.rm = T),
+                 TYPE_RICHNESS = (as.vector(by(conflict_sf$EVENT_TYPE, conflict_sf$grd_id, function(i){length(unique(i))}))),#max(TYPE_RICHNESS, na.rm = T),
+                 SUBTYPE_RICHNESS = (as.vector(by(conflict_sf$SUB_EVENT_TYPE, conflict_sf$grd_id, function(i){length(unique(i))}))),#max(SUBTYPE_RICHNESS, na.rm = T),
+                 ACTOR1_RICHNESS = (as.vector(by(conflict_sf$ACTOR1, conflict_sf$grd_id, function(i){length(unique(na.omit(i)))}))),#max(ACTOR1_RICHNESS, na.rm = T),
+                 ACTOR2_RICHNESS = (as.vector(by(conflict_sf$ACTOR2, conflict_sf$grd_id, function(i){length(unique(na.omit(i)))}))),#,
+                 FATALITIES = (as.vector(by(conflict_sf$FATALITIES, conflict_sf$grd_id, sum))),
+                 pop_dens = (as.vector(by(conflict_sf$pop_dens, conflict_sf$grd_id, mean, na.rm = T)))
 )
 
+aa$EVENTS <- aa$EVENTS/aa$pop_dens
+aa$FATALITIES <- aa$FATALITIES/aa$pop_dens
+aa <- aa[!is.nan(aa$FATALITIES) | !is.infinite(aa$EVENTS) , ]
+aa <- aa[!is.infinite(aa$FATALITIES),]
+aa$pop_dens<- NULL
+
+
+
+bb <- data.frame(id = sort(unique(conflict_sf$grd_id)),
+                 EVENTS =  zscore(as.vector(by(conflict_sf$EVENT_TYPE, conflict_sf$grd_id, length))),#median(EVENTS, na.rm = T),
+                 TYPE_RICHNESS = zscore(as.vector(by(conflict_sf$EVENT_TYPE, conflict_sf$grd_id, function(i){length(unique(i))}))),#max(TYPE_RICHNESS, na.rm = T),
+                 SUBTYPE_RICHNESS = zscore(as.vector(by(conflict_sf$SUB_EVENT_TYPE, conflict_sf$grd_id, function(i){length(unique(i))}))),#max(SUBTYPE_RICHNESS, na.rm = T),
+                 ACTOR1_RICHNESS = zscore(as.vector(by(conflict_sf$ACTOR1, conflict_sf$grd_id, function(i){length(unique(na.omit(i)))}))),#max(ACTOR1_RICHNESS, na.rm = T),
+                 ACTOR2_RICHNESS = zscore(as.vector(by(conflict_sf$ACTOR2, conflict_sf$grd_id, function(i){length(unique(na.omit(i)))}))),#,
+                 FATALITIES = zscore(as.vector(by(conflict_sf$FATALITIES, conflict_sf$grd_id, sum)))
+)
+
+
+
+#' PCA 
+
+
+pca1 <- FactoMineR::PCA(aa[, -1], scale.unit = T, ncp = 2)
+
+plot(pca1)
+
+
+cords <- rbind(pca1$ind$coord) %>% 
+  as_tibble() %>% 
+  dplyr::mutate(dst = sqrt(( Dim.1 - 0)^2 + (Dim.2 - 0 )^2   ),
+                rng = dst > quantile(dst)[3] + IQR(dst)*1.5) 
+
+thr <- cords %>% dplyr::filter(rng) %>% pull(dst) %>% quantile %>% .[4]
+
+#' Assigning weights based distance outside the IQR
+#' wparam weighting parameter
+wparam <- 10
+wh <- cords %>% 
+  dplyr::mutate(wh = ifelse(dst > thr, wparam, 1)) %>% 
+  dplyr::pull(wh)
+
+#' Recalculate PCA with new weights
+pca_w <-  FactoMineR::PCA(aa[, -1], scale.unit = T, ncp =2, graph = T , row.w = wh)
+
+plot(pca_w, choix = "varcor")
+plot(pca_w)
+x_wh <- rbind(pca_w$ind$coord)%>% 
+  dist(., method = "euclidean")
+
+set.seed(1000) #https://towardsdatascience.com/three-versions-of-k-means-cf939b65f4ea
+km_clust <- kmeans(x_wh, centers = 3, iter.max = 1000, nstart = 20, algorithm="MacQueen")#stats::kmeans(x_wh, 3)
+table(km_clust$cluster)
+
+
+#km_clust <- kmeans(dist(pca1$ind$coord[, c(1,2)], method = "euclidean"), centers = 3)
+#table(km_clust$cluster)
+
+plot(pca1$ind$coord[,1], pca1$ind$coord[,2], col = km_clust$cluster)
+
+aa$km_cluster <- km_clust$cluster
+
+aa %>% 
+  dplyr::select(EVENTS:FATALITIES, starts_with("km_")) %>% 
+  dplyr::mutate(km_cluster = as.factor(km_cluster)) %>% 
+  tidyr::pivot_longer(-km_cluster, names_to = "var", values_to = "vals") %>% 
+  ggplot(aes(y= vals, x = km_cluster))+
+  geom_violin(trim=FALSE, fill="gray")+
+  geom_boxplot(width=0.1, fill="white")+
+  facet_wrap(~var, scales = "free")+
+  xlab("")+
+  theme_bw(base_size = 10)+ 
+  scale_y_log10()
+
+#cluster labeling
+
+conf_cluts_vals <- aa %>% 
+  dplyr::select(EVENTS:FATALITIES, starts_with("km_cluster")) %>% 
+  group_by(km_cluster) %>% 
+  dplyr::summarise(m_events = mean(EVENTS)) %>% 
+  arrange(desc(m_events)) %>% 
+  dplyr::mutate(label = c("High conflict", "Moderate conflict", "Limited conflict"),
+                short_label = c("High", "Moderate", "Limited")) %>% 
+  dplyr::mutate(across(everything(.), as.character),
+                label = factor(label, levels = c("High conflict","Moderate conflict",  "Limited conflict")))
+
+aa <- base::merge(aa, conf_cluts_vals , by = "km_cluster", all.x = T ) 
 
 #' merge grid sf with conflict megapixel aggregated variables to consolidate the to_cluster data.frame
 grd_conflict <- base::merge(grd, aa , by = "id", all.x = T ) 
 
-#rm(aa)
+#save results
+#sf::st_write(grd_conflict, paste0(root, "/conflict_clusters_af.geojson"))
+writeVector(vect(grd_conflict), filename = paste0(out,region,'.shp'))
+
+
+
+
+
+
+
+
 
 
