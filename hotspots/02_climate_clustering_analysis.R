e1f4c4dd94a6a598e13e005e443af76fb3b884fb
Mendez <A.C.MENDEZ@cgiar.org>
2023-08-18 08:43:50 -0500

    fixed issue with select_clim_vars funct

--- hotspots/02_climate_clustering_analysis.R
+++ hotspots/02_climate_clustering_analysis.R
@@ -315,6 +315,61 @@ labeling_function <- function(db, n_vars){
   return(list(text_output = ret, var_imp = selected_vars))
 }#end function
 
+select_clim_vars <- function(root, iso, cntr){
+  # Data dictionary
+  
+  dest_dir <- paste0(root ,"/data/",iso,"/_results/cluster_results/climate/climate_selected_variables.csv")
+  
+  if(!file.exists(dest_dir)){
+    
+    vr_tbl <- readxl::read_excel(path = paste0(root,'/Hostpots_data_dictionary.xlsx'), sheet = 1)
+    # Impact pathways (IP) table
+    ip_tbl <- readxl::read_excel(path = paste0(root,'/Africa Climate Security_Country Pathways.xlsx'), sheet = 2)
+    if(!cntr %in% unique(ip_tbl$Country)){
+      cntr <- "Kenya"
+    }
+    ip_tbl <- ip_tbl %>% dplyr::filter(Country == cntr)
+    
+    ## ip_tbl$Values %>% purrr::map(.f = function(txt){base::strsplit(x = txt, split = "\\s{2,}")[[1]]})
+    
+    # Key climate arguments from IP
+    key <- ip_tbl$Values[grep(pattern = '[cC]limate', x = ip_tbl$Dimension)]
+    key <- strsplit(x = key, split = "\\s{2,}") %>% unlist() %>% unique() %>% na.omit() %>% as.character() %>% tolower()
+    
+    # Climate variables from dictionary
+    val <- vr_tbl$Category[vr_tbl$Classification == 'Climate']
+    val <- strsplit(x = val, split = ';')
+    
+    # Function to do the match between key and values
+    select_vars <- function(reference = key, values = val[[6]]){
+      if(all(is.na(values))){ 
+        out <- NA; return(NA)
+      }else{
+        grep2 <- Vectorize(FUN = grep, vectorize.args = 'pattern')
+        out <- unlist(grep2(pattern = values, x = key))
+        if(length(out) > 0){out <- 1} else {out <- NA}
+        return(out)
+      }
+    }
+    
+    selected <- val %>% 
+      purrr::map(.f = function(x){select_vars(reference = key, values = x)}) %>% 
+      unlist()
+    
+    result <- data.frame(Variable = vr_tbl$Variable[vr_tbl$Classification == 'Climate'],
+                         Code     = vr_tbl$Code[vr_tbl$Classification == 'Climate'],
+                         Selected = selected)
+    result <- result %>% tidyr::drop_na() 
+    write.csv(result, dest_dir, row.names = F)
+    
+  } else {
+    result <- read.csv(dest_dir, header = T)
+  }
+  
+  return(result)
+  
+}
+
 
 
 
@@ -322,7 +377,7 @@ labeling_function <- function(db, n_vars){
 root <- '//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO/1.Data/Palmira/CSO/'#dir path to folder data storage
 
 
-country_iso2 <- iso <- "SOM"
+country_iso2 <- iso <- "RWA"
 
 baseDir <- paste0(root, "data/",country_iso2)
 
