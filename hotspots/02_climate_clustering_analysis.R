b4dd254033f51731565acd1ba9890ea6eb0d4ab2
Korir <v.korir@cgiar.org>
2024-02-19 10:30:36 +0300

    --additional instructions after running the script added

--- hotspots/02_climate_clustering_analysis.R
+++ hotspots/02_climate_clustering_analysis.R
@@ -1,7 +1,11 @@
 #########################################
 ## Climatic Clustering #################
 ########################################
+#*PLEASE NOTE: AFTER COMPLETING THE CLUSTERING, 
 
+#*YOU NEED TO MANUALLY ADD 'label' and 'clim_clust_order' COLUMNS IN THE 'clim_cluster_reg_text_description'
+#*
+#################################################################################
 
 g <- gc(reset = T); rm(list = ls()) # Empty garbage collector
 .rs.restartR()                      # Restart R session
