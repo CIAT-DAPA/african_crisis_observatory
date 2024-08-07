093cf3702134c62f4e89aa562c85651cb74ae546
Ogero Derrick <69238003+ogeroderrick@users.noreply.github.com>
2024-08-07 12:17:29 +0300

    Add files via upload
    Updated script on plotting median suitability for pasture types in Karamoja

--- fcm/RSA/karamojamodelling.R
+++ fcm/RSA/karamojamodelling.R
@@ -1,17 +1,3 @@
-#*Pasture Suitability Modelling to inform resource sharing agreement in Karamoja
-#* 
-#* 
-#* Author:: Ogero Derrick
-#* 
-########################################################################
-# Install necessary  packages
-install.packages("geodata")
-install.packages("Recocrop")
-install.packages("tmap")
-install.packages("terra")
-install.packages("sf")
-install.packages("dplyr")
-
 # Load necessary libraries
 library(geodata)
 library(Recocrop)
@@ -21,24 +7,22 @@ library(sf)
 library(dplyr)
 
 # Define working directory
-work_dir <- "D:/OneDrive - CGIAR/SA_Team/Projects/AGNES"
+work_dir <- "D:/OneDrive - CGIAR/CIAT/PastureModelling_Turkana_Case/Coding/Turkana_County_Pasture_Modelling"
 dir.create(work_dir, showWarnings = FALSE, recursive = TRUE)
 setwd(work_dir)
 
 # Define the path to the AOI shapefile
-aoi_shapefile <- "D:/OneDrive - CGIAR/SA_Team/Projects/AGNES/3_Data/1_Raw/igad_cluster_1_1/igad_cluster_1_1.shp"
+aoi_shapefile <- "D:/OneDrive - CGIAR/CIAT/PastureModelling_Turkana_Case/Coding/Turkana_County_Pasture_Modelling/igad_cluster_1_1/igad_cluster_1_1.shp"
 
 # Load AOI shapefile
 aoi <- vect(aoi_shapefile)
 
-# Define ISO codes for the Karamoja cluster(Kenya,Uganda,South Sudan,Ethiopia)
+# Define ISO codes for the Karamoja cluster (Kenya, Uganda, South Sudan, Ethiopia)
 iso_codes <- c("KEN", "UGA", "SSD", "ETH")
 
 # Download climate data for the Karamoja cluster
-rain_list <- lapply(iso_codes, function(iso) 
-  geodata::worldclim_country(iso, var="prec", path="D:/OneDrive - CGIAR/SA_Team/Projects/AGNES/3_Data/1_Raw"))
-tavg_list <- lapply(iso_codes, function(iso) 
-  geodata::worldclim_country(iso, var="tavg", path="D:/OneDrive - CGIAR/SA_Team/Projects/AGNES/3_Data/1_Raw"))
+rain_list <- lapply(iso_codes, function(iso) geodata::worldclim_country(iso, var="prec", path=work_dir))
+tavg_list <- lapply(iso_codes, function(iso) geodata::worldclim_country(iso, var="tavg", path=work_dir))
 
 # Merge the climate data
 rain <- do.call(merge, rain_list)
@@ -57,19 +41,19 @@ grass_types <- c("Cenchrus ciliaris L.", "Chloris gayana", "Eragrostis superba",
                  "Balanites aegyptiaca")
 
 # Load place names for Kenya, Uganda, and South Sudan
-ken_setl <- st_read("D:/OneDrive - CGIAR/SA_Team/Projects/AGNES/3_Data/1_Raw/Place_Names/Kenya/hotosm_ken_populated_places_points_shp.shp") %>%
+ken_setl <- st_read('D:/OneDrive - CGIAR/SA_Team/korir/LULC/hotosm_ken_populated_places_points_shp.shp') %>%
   st_set_crs(st_crs(aoi)) %>%
   st_as_sf() %>%
   st_intersection(st_as_sf(aoi)) %>%
   filter(place == 'town')
 
-uga_setl <- st_read("D:/OneDrive - CGIAR/SA_Team/Projects/AGNES/3_Data/1_Raw/Place_Names/Uganda/hotosm_uga_populated_places_points_shp.shp") %>%
+uga_setl <- st_read('D:/OneDrive - CGIAR/SA_Team/korir/LULC/hotosm_uga_populated_places_points_shp.shp') %>%
   st_set_crs(st_crs(aoi)) %>%
   st_as_sf() %>%
   st_intersection(st_as_sf(aoi)) %>%
   filter(place == 'town')
 
-ssd_setl <- st_read("D:/OneDrive - CGIAR/SA_Team/Projects/AGNES/3_Data/1_Raw/Place_Names/South_Sudan/ssd_pppls_ocha_20221216.shp") %>%
+ssd_setl <- st_read('D:/OneDrive - CGIAR/SA_Team/korir/LULC/SSD_PopulatedAreas_Dataset/ssd_pppls_ocha_20221216.shp') %>%
   st_set_crs(st_crs(aoi)) %>%
   st_as_sf() %>%
   st_intersection(st_as_sf(aoi))
@@ -97,7 +81,7 @@ run_and_save_ecocrop <- function(grass_name, rain, tavg, aoi, work_dir, ken_setl
   hv_clipped <- crop(hv, aoi)
   print(hv_clipped)
   # Create a subdirectory for each grass type
-  grass_dir <- file.path("D:/OneDrive - CGIAR/SA_Team/Projects/AGNES/3_Data/3_Outputs","suitability", gsub(" ", "_", grass_name))
+  grass_dir <- file.path(work_dir, "models", gsub(" ", "_", grass_name))
   dir.create(grass_dir, showWarnings = FALSE, recursive = TRUE)
   print(grass_dir)
   
@@ -106,7 +90,7 @@ run_and_save_ecocrop <- function(grass_name, rain, tavg, aoi, work_dir, ken_setl
   
   # Create the thematic map
   them_map <- tm_shape(hv_clipped) +
-    tm_raster(style = "cat", palette = "Greens", title = "Suitability Index",legend.show = TRUE) +
+    tm_raster(style = "cat", palette = "Greens", title = "Suitability Index", legend.show = TRUE) +
     tm_shape(aoi_sf) +
     tm_borders(col = "red", lwd = 2) +
     tm_shape(ken_setl) + tm_text("name", size = 0.3, col = "black", remove.overlap = T) +
@@ -114,7 +98,7 @@ run_and_save_ecocrop <- function(grass_name, rain, tavg, aoi, work_dir, ken_setl
     tm_shape(ssd_setl) + tm_text("featureNam", size = 0.3, col = "black",remove.overlap = T) +
     tm_layout(
       title = paste("Suitability Map for", grass_name, "in Karamoja"),
-      title.size = 2.0,
+      title.size = 1.5,
       title.position = c("center", "top"),
       legend.outside = TRUE,
       legend.outside.position = "right",
@@ -127,14 +111,13 @@ run_and_save_ecocrop <- function(grass_name, rain, tavg, aoi, work_dir, ken_setl
     tm_graticules(n.x = 6, n.y = 6, lines = TRUE, labels.size = 0.6)
   them_map
   # Save the suitability map as JPEG
-  tmap_save(them_map, file.path(grass_dir,paste0("suitability_map_", gsub(" ", "_", grass_name), ".jpg")), width = 1200, height = 800, dpi = 300)
+  tmap_save(them_map, file.path(grass_dir, paste0("suitability_map_", gsub(" ", "_", grass_name), ".jpg")), width = 1200, height = 800, dpi = 300)
 }
 
 # Loop through each grass type and run the model
 for (grass_name in grass_types) {
   tryCatch({
-    run_and_save_ecocrop(grass_name, rain_aoi, tavg_aoi, aoi, 
-                         work_dir, ken_setl, uga_setl, ssd_setl)
+    run_and_save_ecocrop(grass_name, rain_aoi, tavg_aoi, aoi, work_dir, ken_setl, uga_setl, ssd_setl)
   }, error = function(e) {
     message("Error in processing ", grass_name, ": ", e$message)
   })
