281667a4e71a5141bd4d7295e44ab0f61b18b6c2
bensonkenduiywo <bensonkemboi@gmail.com>
2024-10-04 03:26:05 -0700

    --reorganized to extract months only

--- auxiliary/Extract_SPEI.R
+++ auxiliary/Extract_SPEI.R
@@ -12,40 +12,60 @@ library(terra)
 library(sf)
 
 monthly_spei <- "//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO2/FCM/Data/intermediate/spei/monthly_spei"
-coords_csv <- "C:/Users/bchepngetich/Downloads/KEN_2024_coordinates.csv"
-kenya <- sf::st_read("C:/Users/bchepngetich/Downloads/kenya_shp/Kenya_county_dd.shp")
+#coords_csv <- "C:/Users/bchepngetich/Downloads/KEN_2024_coordinates.csv"
+
+kenya <- vect("//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO2/FCM/Data/raw/HH/KEN_2024_coordinates.shp")
 
 # read csv containing coordinates list
-coords <- read.csv(coords_csv)
-coords <- coords[, c("longnum","latnum")]
-spei_coords <- coords
+#coords <- read.csv(coords_csv)
+#coords <- coords[, c("longnum","latnum")]
+#spei_coords <- coords
 # list spei raster files 
 spei <- list.files(monthly_spei, pattern = "\\.tif$", full.names = TRUE)
 
-years <- 2003:2022
-for (year in years) {
-  yr <- as.character(year)
-  print(yr)
-  year_files <- grep(yr, spei, value = TRUE)
-  print(year_files)
-  year_files <- lapply(year_files, rast)
-  print("rast done")
-  year_files <- do.call(c, year_files)
-  year_files <- terra::mask(year_files, kenya)
-  print("done mask")
-  x <- 1
-  for (i in seq(1,nlyr(year_files), by=3)){
-    print(x)
-    quarter <- year_files[[i:(i+2)]]
-    quarter <- app(quarter, mean)
-    names(quarter) <- paste0(yr,"-",x)
-    spei_values <- extract(quarter, coords)
-    spei_coords <- cbind(spei_coords, spei_values)
-    x <- x + 1
-    print("done")
-  }
+years <- c("2003", "2008", "2009", "2014", "2022")
+year_files <- grep(years, spei, value = TRUE)
+selectFiles <- function(years){
+  grep(years, spei, value = TRUE)
 }
-id_cols <- grep("ID", colnames(spei_coords))
-final <- spei_coords[,-id_cols]
-write.csv(final, "C:/Users/bchepngetich/Downloads/SPEI_Values.csv")
+files <- lapply(years, selectFiles)
+img <- rast(unlist(files))
+df <- terra::extract(img, kenya, bind=T)
+# 
+# dff <- reshape(df, 
+#               direction = "long",
+#               varying = list(names(df)[-c(1:3)]),
+#               v.names = "Spei",
+#               idvar = c("hv001"),
+#               timevar = "YearMonth",
+#               times= 1:length(names(df)[-c(1:3)])
+#               )
+# for (year in years) {
+#   yr <- as.character(year)
+#   print(yr)
+#   year_files <- grep(yr, spei, value = TRUE)
+#   print(year_files)
+#   year_files <- lapply(year_files, rast)
+#   print("rast done")
+#   year_files <- do.call(c, year_files)
+#   year_files <- terra::mask(year_files, kenya)
+#   print("done mask")
+#   x <- 1
+#   #for (i in seq(1,nlyr(year_files), by=3)){
+#   for (i in seq(1,nlyr(year_files), by=3)){
+#     print(x)
+#     quarter <- year_files[[i:(i+2)]]
+#     quarter <- app(quarter, mean)
+#     names(quarter) <- paste0(yr,"-",x)
+#     spei_values <- extract(quarter, coords)
+#     #spei_coords <- cbind(spei_coords, spei_values)
+#     x <- x + 1
+#     print("done")
+#   }
+# }
+
+
+#id_cols <- grep("ID", colnames(spei_coords))
+#final <- spei_coords[,-id_cols]
+write.csv(df, "//alliancedfs.alliance.cgiar.org/WS18_Afrca_K_N_ACO2/FCM/Data/raw/HH/SPEI_Values.csv")
 
